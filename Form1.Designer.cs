3dfe7b08dc195b933a33e651c3a1046b5ffe002e
Kamil Tele <szlyrda@gmail.com>
2023-07-07 10:24:19 +0200

    Adding all files

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Serializacja
+{
+    partial class Serializacja
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(36, 142);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(36, 188);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(175, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Zwierze";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "waga";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "wysokość";
+            this.label3.Visible = false;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(36, 234);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(175, 51);
+            this.addButton.TabIndex = 9;
+            this.addButton.Text = "Dodaj (serializuj)";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Visible = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click_1);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(467, 52);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(282, 357);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "Tutaj pojawi się lista";
+            this.textBox4.Visible = false;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(467, 415);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(282, 23);
+            this.showButton.TabIndex = 11;
+            this.showButton.Text = "Pokaż z pliku (deserializuj)";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Visible = false;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(276, 52);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(185, 71);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "Tutaj pojawi się ostatni dodany rekord";
+            this.textBox5.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Wybierz miejsce zapisu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Serializacja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Serializacja";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Serializacja do pliku xml";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace Serializacja
+{
+    public partial class Serializacja : Form
+    {
+        public Serializacja()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        string folderPath;
+        public void button1_Click(object sender, EventArgs e)
+        {
+            // Okno dialogowe wyboru folderu
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+
+            // Ustawienie parametrów okna dialogowego
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+
+            // Jeśli użytkownik wybierze folder, zapisz jego ścieżkę
+            folderBrowser.FileName = "Folder Selection.";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = Path.GetDirectoryName(folderBrowser.FileName);
+            }
+
+            // Wyświetlenie pól tekstowych i przycisków
+            textBox4.Visible = true;
+            showButton.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            textBox5.Visible = true;
+            addButton.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+        }
+        
+        private void addButton_Click_1(object sender, EventArgs e)
+        {
+            // Utworzenie nowego obiektu Animal na podstawie wprowadzonych danych
+            string animalName = textBox1.Text;
+            double animalWeight = Convert.ToDouble(textBox2.Text);
+            double animalHeight = Convert.ToDouble(textBox3.Text);
+            Animal animal = new Animal(animalName, animalWeight, animalHeight);
+
+            // Dodanie nowego zwierzęcia do listy theAnimals
+            theAnimals.Add(animal);
+
+            // Wyświetlenie informacji o dodanym zwierzęciu
+            textBox5.Text = animal.ToString();
+
+            // Serializacja listy theAnimals i zapisanie jej do pliku XML
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Animal>));
+            using (TextWriter tw = new StreamWriter(Path.Combine(folderPath, "zwierzeta.xml")))
+            {
+                serializer.Serialize(tw, theAnimals);
+            }
+            
+        }
+
+        // Inicjalizacja listy theAnimals
+        List<Animal> theAnimals = new List<Animal>
+        {
+   
+        };
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Deserializacja pliku XML i wczytanie danych do listy theAnimals
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Animal>));
+            using (TextReader tr = new StreamReader(Path.Combine(folderPath, "zwierzeta.xml")))
+            {
+                theAnimals = (List<Animal>)serializer.Deserialize(tr);
+            }
+
+            // Wyczyszczenie pola tekstowego textBox4
+            textBox4.Clear();
+
+            // Wyświetlenie informacji o zwierzętach z listy theAnimals
+            foreach (Animal animal in theAnimals)
+            {
+                animal.AnimalID++;
+                textBox4.AppendText(animal.ToString());
+                textBox4.AppendText(Environment.NewLine);
+                
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+
+    // Deklaracja klasy Animal, która implementuje interfejs ISerializable
+    [Serializable()]
+    public class Animal : ISerializable
+    {
+
+        // Deklaracja właściwości Name, Weight, Height i AnimalID
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public int AnimalID { get; set; }
+
+        // Konstruktor bezparametrowy
+        public Animal() { }
+
+        // Konstruktor z trzema parametrami, z wartościami domyślnymi
+        public Animal(string name = "No Name",
+            double weight = 0,
+            double height = 0)
+        {
+            // Inicjalizacja właściwości obiektu
+            Name = name;
+            Weight = weight;
+            Height = height;
+        }
+
+        // Metoda dodająca nowe zwierzę
+        public void addAnimal(Animal animal)
+        {
+            animal.addAnimal(this);
+        }
+
+        // Metoda ToString, która zwraca informacje o zwierzęciu w postaci tekstu
+        public override string ToString()
+        {
+            return string.Format("{0} waży: {1} kg i ma {2} cm wysokości", Name, Weight, Height);
+        }
+
+        // Metoda GetObjectData, która zapisuje dane obiektu do obiektu SerializationInfo
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("name", Name);
+            info.AddValue("weight", Weight);
+            info.AddValue("height", Height);
+        }
+
+        // Konstruktor przyjmujący dwa parametry, obiekt SerializationInfo i StreamingContext, który odtwarza obiekt zapisany w pliku
+        public Animal(SerializationInfo info, StreamingContext context)
+        {
+            // Odczytanie wartości właściwości obiektu z obiektu SerializationInfo
+            Name = (string)info.GetValue("name", typeof(string));
+            Weight = (double)info.GetValue("weight", typeof(double));
+            Height = (double)info.GetValue("height", typeof(double));
+        }
+    }
+}
