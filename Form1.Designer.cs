5ab8a9953ff5071357f81abfb6262fee84de85b3
Kamil Tele <szlyrda@gmail.com>
2023-07-07 11:20:51 +0200

    Changing UI for better experince

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(36, 94);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(175, 20);
+            this.textBox1.Size = new System.Drawing.Size(175, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.Visible = false;
             // 
@@ -53,7 +53,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(36, 142);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(175, 20);
+            this.textBox2.Size = new System.Drawing.Size(175, 22);
             this.textBox2.TabIndex = 1;
             this.textBox2.Visible = false;
             // 
@@ -61,7 +61,7 @@
             // 
             this.textBox3.Location = new System.Drawing.Point(36, 188);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(175, 20);
+            this.textBox3.Size = new System.Drawing.Size(175, 22);
             this.textBox3.TabIndex = 2;
             this.textBox3.Visible = false;
             // 
@@ -70,7 +70,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(36, 78);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.Size = new System.Drawing.Size(53, 16);
             this.label1.TabIndex = 3;
             this.label1.Text = "Zwierze";
             this.label1.Visible = false;
@@ -81,9 +81,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(36, 126);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 4;
-            this.label2.Text = "waga";
+            this.label2.Text = "Waga";
             this.label2.Visible = false;
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
@@ -92,19 +92,23 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(36, 172);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.Size = new System.Drawing.Size(74, 16);
             this.label3.TabIndex = 5;
-            this.label3.Text = "wysokość";
+            this.label3.Text = "Wysokość ";
             this.label3.Visible = false;
             // 
             // addButton
             // 
+            this.addButton.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.addButton.FlatAppearance.BorderSize = 0;
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.addButton.Location = new System.Drawing.Point(36, 234);
             this.addButton.Name = "addButton";
             this.addButton.Size = new System.Drawing.Size(175, 51);
             this.addButton.TabIndex = 9;
             this.addButton.Text = "Dodaj (serializuj)";
-            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.UseVisualStyleBackColor = false;
             this.addButton.Visible = false;
             this.addButton.Click += new System.EventHandler(this.addButton_Click_1);
             // 
@@ -122,12 +126,16 @@
             // 
             // showButton
             // 
+            this.showButton.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.showButton.FlatAppearance.BorderSize = 0;
+            this.showButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.showButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.showButton.Location = new System.Drawing.Point(467, 415);
             this.showButton.Name = "showButton";
             this.showButton.Size = new System.Drawing.Size(282, 23);
             this.showButton.TabIndex = 11;
             this.showButton.Text = "Pokaż z pliku (deserializuj)";
-            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.UseVisualStyleBackColor = false;
             this.showButton.Visible = false;
             this.showButton.Click += new System.EventHandler(this.showButton_Click);
             // 
@@ -144,18 +152,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 39);
+            this.button1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.Size = new System.Drawing.Size(208, 27);
             this.button1.TabIndex = 13;
             this.button1.Text = "Wybierz miejsce zapisu";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Serializacja
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox5);
@@ -168,6 +180,8 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Serializacja";
